7cf1cb634699629e067129adf5f3e4c8ca27e476
mmaalin <maalin.622139@swl.k12.oh.us>
2024-10-17 08:21:57 -0400

    fin

--- src/NestedLoops.java
+++ src/NestedLoops.java
@@ -1,5 +1,30 @@
-public class Main {
+public class NestedLoops {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 0; i<6; i++) {
+            for (int j = 0; j<i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i<5; i++) {
+            for (int j = 5; j>i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        for (int i = 0; i<5; i++) {
+            for (int j = 0; j<5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
 }
